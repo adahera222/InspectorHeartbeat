8d70f122c46adea5561d71ee863848c65093f2c7
Lucas Caro <lucascaro@gmail.com>
2013-01-27 04:29:37 -0300

    Added music and timer

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,15 +4,31 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 	
 	public string nextScene;
+	public float allowedTime;
+	public AudioClip randomClip;
+	
+	protected TextMesh textMesh;
 	
 	// Use this for initialization
 	void Start () {
-	
+		textMesh = GetComponentInChildren<TextMesh>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		allowedTime -= Time.deltaTime;
+		if (allowedTime <= 0) {
+			textMesh.text = "00:00";
+			//GameObject.FindGameObjectWithTag("Player").SendMessage("AimedAndFailed");
+			PlayerLost();
+		}
+		else {
+			int minutes = Mathf.FloorToInt(allowedTime / 60F);
+			int seconds = Mathf.FloorToInt(allowedTime - minutes * 60);
+			string time = string.Format("{0:00}:{1:00}", minutes, seconds);
+			Debug.Log(time);
+			textMesh.text = time;
+		}
 	}
 	
 	void NextScene() {
